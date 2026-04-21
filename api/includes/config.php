c4106aa5801bc39372bf07f73904810c715c242c
gounderdivakaran-boop <gounderdivakaran@gmail.com>
2026-04-22 02:12:39 +0530

    debug: re-enable errors to identify site failure

--- api/includes/config.php
+++ api/includes/config.php
@@ -46,8 +46,8 @@ if (!$con) {
 
 // Environment-aware error reporting
 if (isset($_SERVER['VERCEL']) || (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] !== 'localhost')) {
-    error_reporting(0);
-    ini_set('display_errors', 0);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+    ini_set('display_errors', 1);
 } else {
     ini_set('display_errors', 1);
     ini_set('log_errors', 1);
